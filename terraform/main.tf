a35688759baae707445ffdd5a78e5f826065ab5b
shriharshan <shriharshan123@gmail.com>
2026-02-06 15:30:20 +0530

    feat: Add subscription filter to wire up demo app logs

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,6 +32,17 @@ module "lambda" {
   demo_function_name = var.demo_function_name
 }
 
+data "aws_caller_identity" "current" {}
+
+module "subscription" {
+  source = "./modules/subscription"
+
+  commander_function_name = module.lambda.function_name
+  commander_lambda_arn    = module.lambda.function_arn
+  demo_log_group_name     = var.demo_log_group
+  demo_log_group_arn      = "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:${var.demo_log_group}"
+}
+
 output "function_name" {
   value = module.lambda.function_name
 }
